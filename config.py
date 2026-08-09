5600fd70e2524268b1978cc6ae241571347a5492
lethingochon <lethingochon.hoabinh@gmail.com>
2026-08-09 22:15:12 +0700

    Fix database config

--- config.py
+++ config.py
@@ -1,8 +1,12 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 class Config:
     SECRET_KEY = "digiteacher-secret-key"
 
-    SQLALCHEMY_DATABASE_URI = (
-        "mysql+pymysql://root:123456@localhost/digiteacher"
-    )
+    # Kết nối chuẩn tới file SQLite dgnls.db trong thư mục instance
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'instance', 'dgnls.db')
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False

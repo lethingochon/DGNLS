7784cc4f5b17fd94bdda0b52f4237422470a1031
lethingochon <lethingochon.hoabinh@gmail.com>
2026-08-09 22:46:09 +0700

    Khoi phuc nguyen ban MySQL

--- config.py
+++ config.py
@@ -1,12 +1,10 @@
 import os
 
-basedir = os.path.abspath(os.path.dirname(__file__))
-
 class Config:
     SECRET_KEY = "digiteacher-secret-key"
-
-    # Kết nối chuẩn tới file SQLite dgnls.db trong thư mục instance
+    
+    # Khôi phục nguyên bản kết nối MySQL
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'instance', 'dgnls.db')
-
+        'mysql+pymysql://root:123456@localhost/dgnls'
+        
     SQLALCHEMY_TRACK_MODIFICATIONS = False

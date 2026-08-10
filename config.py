b431a6014b005e07a10d877a9bbc698fb50f7274
lethingochon <168560996+lethingochon@users.noreply.github.com>
2026-08-10 13:08:00 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dgnls-thpt-sec-key-2026'
-    # Nhận chuỗi kết nối từ biến môi trường, mặc định dùng SQLite nếu không có
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///dgnls.db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'dgnls.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
